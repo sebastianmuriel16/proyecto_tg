44a6cf8c4e1c1a1c03dbdb9e3611667e80cba4fd
sebastianmuriel16 <muriel.sebastian@correounivalle.edu.co>
2023-09-20 22:48:43 -0500

    comentarios de los usuarios en las publicaciones

--- app/src/main/java/com/example/prototipo_canino/HomeFragment.kt
+++ app/src/main/java/com/example/prototipo_canino/HomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -18,9 +19,11 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 
 
@@ -102,6 +105,23 @@ class HomeFragment : Fragment(), FragmentAux {
                         }else{
                             View.INVISIBLE
                         }
+                        //Comentarios
+                        mSnapshotsRef.child(snapshot.id).child("comentarios")
+                            .addListenerForSingleValueEvent(object : ValueEventListener{
+                                override fun onDataChange(dataSnapshot: DataSnapshot){
+                                    val listaComentarios = dataSnapshot.children.map{ comentarioSnapshot ->
+                                        comentarioSnapshot.getValue() as Map<String, String>
+                                    }
+                                    val comentarioAdapter = comentarioAdapter(listaComentarios)
+                                    comentariosRecyclerView.adapter = comentarioAdapter
+                                    comentariosRecyclerView.layoutManager = LinearLayoutManager(mContext)
+                                }
+
+                                override fun onCancelled(databaseError: DatabaseError ){
+                                    //manejo de errores
+                                }
+                            })
+
                     }
 
                 }
@@ -191,8 +211,62 @@ class HomeFragment : Fragment(), FragmentAux {
                 cbLike.setOnCheckedChangeListener { _, checked ->
                     setLike(snapshot, checked)
                 }
+
+                btnComentar.setOnClickListener {
+                    val comentario = etInputComentario.text.toString().trim()
+                    if (comentario.isNotEmpty()){
+                        publicarComentario(snapshot,comentario)
+                        etInputComentario.text.clear()
+                    }
+                    else{
+                        Snackbar.make(binding.root, "Por favor, escribe un comentario.", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
             }
         }
     }
+
+    //comentatios
+    inner class comentarioHolder(view:View) : RecyclerView.ViewHolder(view){
+        val tvComentarioUsuario: TextView = view.findViewById(R.id.tvNombre_Usuario)
+        val tvComentario: TextView = view.findViewById(R.id.tvComentario)
+    }
+
+    inner class comentarioAdapter(private val comentarios: List<Map<String,String>>):
+    RecyclerView.Adapter<comentarioHolder>(){
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): comentarioHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_comments,parent,false)
+            return  comentarioHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: comentarioHolder, position: Int) {
+            val comentario = comentarios[position]
+            holder.tvComentarioUsuario.text = comentario["userName"]
+            holder.tvComentario.text = comentario["comentario"]
+        }
+
+        override fun getItemCount(): Int {
+            return comentarios.size
+        }
+    }
+
+    //metodos para subir comentarios
+    private fun publicarComentario(snapshot: Snapshot,comentario:String){
+        val comentarioRef = mSnapshotsRef.child(snapshot.id)
+            .child("comentarios")
+            .push()
+
+        val comentarioData = mapOf(
+            "userId" to SnapshotsApplication.currentUser.uid,
+            "userName" to SnapshotsApplication.currentUser.displayName,
+            "comentario" to comentario
+        )
+        comentarioRef.setValue(comentarioData)
+    }
+
+
+
 
 }
